068c2e614308aae0a3a4f31d38335bd493346a78
Skyyy <81206121+Skyyy-com@users.noreply.github.com>
2022-01-25 19:51:22 +0800

    Add files via upload

---
+++ converter.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "[1] binary to decimal"
+echo "[2] decimal to binary"
+echo "[3] hex to decimal"
+echo "[4] decimal to hex"
+echo "[5] binary to hex"
+echo "[6] hex to binary"
+
+echo ""
+
+echo "select conversion method"
+read CHOICE
+echo "select value to convert"
+read val
+
+#choice 1
+
+[ $CHOICE = "1" ] && echo "$((2#$val))"
+
+#choice 2
+
+[ $CHOICE = "2" ] && echo "obase=2;$val" | bc
+
+#choice 3
+
+[ $CHOICE = "3" ] && echo "$((16#$val))" | bc
+
+#choice 4
+
+[ $CHOICE = "4" ] && echo "obase=16;$val" | bc
+
+#choice 5
+
+[ $CHOICE = "5" ] && printf '%x\n' "$((2#$val))"
+
+#choice 6
+
+[ $CHOICE = "6" ] && echo "obase=2;ibase=16;$val" | bc
